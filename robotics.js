38814d3e83bf872fc9dc5c280e2c8d05662301c0
CodeChanning <channinggaddy@gmail.com>
2022-10-02 16:20:13 -0400

    first commit

---
+++ robotics.js
@@ -0,0 +1,6 @@
+console.log("pre id");
+const robotImg = document.getElementById('starter-div');
+console.log("post id", document.getElementsByTagName('img'));
+console.log("lmao", document.getElementById('g'));
+robotImg.src = WLR_CAT;
+console.log("posst source");
